eaaee28cc8b063a0a6993cc6f6b930628c7027f6
Shiva kumar <166685858+Shiva-OC@users.noreply.github.com>
2024-08-19 11:23:45 +0530

    Update app_startup.sh

--- app_startup.sh
+++ app_startup.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+# Create the .streamlit directory if it doesn't exist
 mkdir -p .streamlit/
+
+# Create or overwrite the config.toml file with server settings
 echo "[server]" > .streamlit/config.toml
 echo "baseUrlPath = \"$INGRESS_PREFIX\"" >> .streamlit/config.toml
+
+# Append the theme settings to the config.toml file
+echo "[theme]" >> .streamlit/config.toml
+echo "base = \"dark\"" >> .streamlit/config.toml
+
+# Run the Streamlit application
 streamlit run app.py --server.port=8501 --server.address=0.0.0.0
